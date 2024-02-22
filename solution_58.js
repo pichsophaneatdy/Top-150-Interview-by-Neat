074cd4ee9bb5c0b956292d242fe137ae00690c2f
Neat <sophaneat@student.fdu.edu>
2024-02-22 12:25:54 -0800

    Add Question and Solution for Question 58

---
+++ solution_58.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    // Remove white space at the beginnning and at the end
+    let trimS = s.trim();
+    // count
+    let count = 0;
+    // Loop from the end of the string
+    for(let i = trimS.length - 1; i >=0; i--) {
+        if(trimS[i] === " ") {
+            break;
+        } else {
+            count++;
+        }
+    }
+    return count;
+};
