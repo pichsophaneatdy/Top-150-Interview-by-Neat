cd6815717ab492473ad7e1babffee904503abcd1
Neat <sophaneat@student.fdu.edu>
2024-03-02 00:24:51 -0800

    Add desc and solution for question 8

---
+++ Day8_PalindromeNumber/solution_8.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    x = x.toString();
+    let i = 0;
+    let j = x.length - 1;
+    while(i < j) {
+        if(x[i] !== x[j]) return false;
+        i++;
+        j--;
+    }
+    return true;
+};
